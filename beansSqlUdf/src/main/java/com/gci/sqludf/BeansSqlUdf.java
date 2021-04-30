8eabdcd273e0ea64c68b79fd4f671b06e63d34ff
yangsenbin@gci-china.com <yangsenbin@gci-china.com>
2021-04-30 17:23:55 +0800

    Java编写自定义impala函数

---
+++ beansSqlUdf/src/main/java/com/gci/sqludf/BeansSqlUdf.java
@@ -0,0 +1,32 @@
+package com.gci.sqludf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import java.security.MessageDigest;
+
+public class BeansSqlUdf extends UDF{
+    public static void main(String[] args) {
+        String hello = "123456789";
+        System.out.println("MD5加密后的结果：" + evaluate(hello));
+    }
+
+    public static String evaluate(String value) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] bytes = messageDigest.digest(value.getBytes());
+            for (int i = 0; i < bytes.length; i++) {
+                int tempInt = bytes[i] & 0xff;
+                if (tempInt < 16) {
+                    sb.append(0);
+                }
+                sb.append(Integer.toHexString(tempInt));
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return sb.toString();
+    }
+}
+
+///user/yangsenbin/project/get_all_od
---
+++ beansSqlUdf/src/main/java/com/gci/sqludf/MatchCity.java
@@ -0,0 +1,37 @@
+package com.gci.sqludf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class MatchCity extends UDF{
+    public static void main(String[] args) {
+        String address = "新疆维吾尔自治区曹尼玛市天河区棠下街道中山大道西大舜商务中心(科新路)";
+        System.out.println("city：" + evaluate(address));
+    }
+
+    public static String evaluate(String address) {
+        try{
+            int length = address.length();
+            int fromIdx = -1; //截取市名称开始的那一位
+            int toIdx = -1; // 截取市结束的那一位
+            for (int i = 0; i < length; i++) {
+                String addressi = address.substring(i,i+1);
+                if (addressi.equals("省")|(addressi.equals("区"))){
+                    fromIdx = i+1;
+                }
+                if (addressi.equals("市")){
+                    toIdx = i+1;
+                    break;
+                }
+
+            }
+
+            String city = address.substring(fromIdx, toIdx);
+            return city;
+        }catch (Exception e){
+            return "广州市";  //如果报错，就默认广州市
+        }
+
+
+
+    }
+}
